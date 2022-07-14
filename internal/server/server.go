8a7e870d22735b0c6663c1125ffeb498dabfff02
Avyukth <makur.subhrajit@gmail.com>
2022-07-14 17:03:54 +0530

    added go mod

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,6 +6,11 @@ import (
 	"os"
 	"time"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
 	"golang.org/x/net/websocket"
 )
 
@@ -20,11 +25,17 @@ func Run() {
   if *address == ":" {
     *address = ":8080"
   }
-  
+  engine:=html.New("./views", ".html")
+  app := fiber.New(fiber.Config{Views: engine})
+  app.Use(cors.New())
+  app.Use(logger.New())
+
+
   app.Get("/", handlers.welcome)
   app.Get("/room/create", handlers.RoomCreate)
   app.Get("/room/:uuid", handlers.Room)
-  app.Get("/room/:uuid/websocket",)
+  app.Get("/room/:uuid/websocket",websocket.New(handlers.RoomWebsocket, websocket.Config{
+    HandshakeTimeout: time.Second * 10,}))
   app.Get("/room/:uuid/chat",handlers.RoomChat)
   app.Get("/room/:uuid/chat/websocket",websocket.RoomChatWebsocket)
   app.Get("/room/:uuid/viewer/websocket",handlers.RoomViewerWebsocket)
