711abb6616ab0a949369069385521867a22e925b
Avyukth <makur.subhrajit@gmail.com>
2022-07-15 01:13:28 +0530

    peers changed

--- pkg/webrtc/peers.go
+++ pkg/webrtc/peers.go
@@ -1,5 +1,20 @@
 package webrtc
 
+import(
+	
+)
+
+type Room struct {
+	Peers *Peers
+	Hub *chat.Hub
+
+}
+type Peers struct {
+	ListLock sync.RwMutex
+	Connection  []PeerConnectionState
+	TrackLocals  map[string]*webrtc.TrackLocalStaticRTP
+}
+
 func (p *Peer) DispatchKeyFrames() {
 	p.mu.Lock()
 	defer p.mu.Unlock()
