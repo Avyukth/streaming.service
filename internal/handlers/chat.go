12d086ac9594e3d338dfeb4d4414ba705fed4f77
Avyukth <makur.subhrajit@gmail.com>
2023-07-23 02:20:30 +0530

    adding stream web socket

--- internal/handlers/chat.go
+++ internal/handlers/chat.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 	guid "github.com/google/uuid"
+	"github.com/sqs/goreturns/returns"
 )
 
 func RoomChat(c *fiber.Ctx) error {
@@ -15,7 +16,7 @@ func RoomChatWebsocket(c *websocket.Conn) {
 	uuid := c.Params("uuid")
 
 	if uuid == "" {
-		retturn
+		return
 
 	}
 	w.RoomsLock.LocK()
@@ -29,4 +30,26 @@ func RoomChatWebsocket(c *websocket.Conn) {
 		return
 	}
 	chat.PeerChatConn(c.Conn, room.Hub)
+}
+
+func StreamChatWebSocket(c *websocket.Conn) {
+	suuid := c.Params("suuid")
+	if suuid == "" {
+		return
+	}
+	w.RoomsLock.Lock()
+	if stream.Hub == nil {
+		return
+	}
+	if stream, ok := w.Streams[suuid]; ok {
+
+		w.RoomsLock.Unlock()
+		if stream.Hub == nil {
+			hub := chat.NewHub()
+
+			stream.Hub = hub
+			go hub.Run()
+		}
+
+	}
 }
